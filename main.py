4bd714aeab6635d31253713080bfc696cd76fe1d
abhiyada01 <60868403+abhiyada01@users.noreply.github.com>
2021-12-03 12:05:38 +0530

    Modified

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 # This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QStandardItemModel
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Ui(QMainWindow):
+    FROM, SUBJECT, DATE = range(3)
+
+    def __init__(self):
+        super(Ui, self).__init__()
+        self.setWindowState(Qt.WindowMaximized)
+        loadUi('basic_ui.ui', self)
+        self.acc_box.addItem("Administration")
+        self.acc_box.addItem("AIO")
+        self.acc_box.addItem("TCL")
+        self.acc_box.addItem("PREWAVE")
+        self.acc_box.addItem("POSTWAVE")
+        self.acc_box.addItem("TESTING")
+        self.acc_box.addItem("COATING")
+        self.acc_box.addItem("DISPATCH")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class OtherWindow(QWidget):
+    def __init__(self):
+        pass
+
+
+def Application():
+    app = QApplication(sys.argv)
+    window = Ui()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    Application()
