055d2d6ab600704e5a97b1620dee3284451ddd72
abhiyada01 <60868403+abhiyada01@users.noreply.github.com>
2021-12-03 12:56:21 +0530

    Modified

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # This is a sample Python script.
 import sys
-
-from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QStandardItemModel
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 
 
 class Ui(QMainWindow):
-    FROM, SUBJECT, DATE = range(3)
 
     def __init__(self):
         super(Ui, self).__init__()
         self.setWindowState(Qt.WindowMaximized)
         loadUi('basic_ui.ui', self)
+        self.drop_down_setting()
+        self.table_setting()
+
+    def drop_down_setting(self):
         self.acc_box.addItem("Administration")
         self.acc_box.addItem("AIO")
         self.acc_box.addItem("TCL")
@@ -24,11 +24,18 @@ class Ui(QMainWindow):
         self.acc_box.addItem("COATING")
         self.acc_box.addItem("DISPATCH")
 
-
-
-class OtherWindow(QWidget):
-    def __init__(self):
-        pass
+    def table_setting(self):
+        col_width = 170
+        self.data_list.setColumnWidth(0, 350)
+        self.data_list.setColumnWidth(1, col_width)
+        self.data_list.setColumnWidth(2, col_width)
+        self.data_list.setColumnWidth(3, col_width)
+        self.data_list.setColumnWidth(4, col_width)
+        self.data_list.setColumnWidth(5, col_width)
+        self.data_list.setColumnWidth(6, col_width)
+        self.data_list.setColumnWidth(7, col_width)
+        self.data_list.setColumnWidth(8, col_width)
+        self.data_list.setColumnWidth(0, col_width)
 
 
 def Application():
